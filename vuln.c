6d5d97a5464ece84ed7b61c4a91723fb72ebd23f
waseeq14 <waseeq2003@gmail.com>
2024-11-19 14:40:38 +0000

    last good before func tracking

---
+++ vuln.c
@@ -0,0 +1,26 @@
+void enter_name(){
+    char name[64];
+    puts("Please enter your name:");
+    fgets(name, sizeof(name), stdin);
+    printf("Hello ");
+    printf(name);
+}
+
+void vuln(){
+    char buffer[256];
+    fgets(buffer,sizeof(buffer),stdin);
+}
+
+int main()
+{
+    setuid(0);
+    setgid(0);
+
+    enter_name();
+
+    puts("\nGood luck with your ret2libc, you'll never bypass my new PIE protection OR find out where my lib-c library is :P\n");
+
+    vuln();
+
+    return 0;
+}
