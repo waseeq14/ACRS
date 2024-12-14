3a7af0c5a654c0e928d579571de2444cfdc74b80
ShSAdil <sheikhadi077@gmail.com>
2024-12-14 15:51:06 +0500

    change: Display Texts in Loading Page

--- frontend/src/routes/Loading/index.js
+++ frontend/src/routes/Loading/index.js
@@ -3,15 +3,29 @@ import { useNavigate } from 'react-router'
 
 import styles from './styles.module.css'
 
+const displayTexts = [
+  'Connecting to the DOOM server',
+  'KLEEning your exploit history',
+  'Teaching LLM 2+2',
+  'Initializing pwn frameworks',
+  'Initiating suffering',
+  'Applying bandage to patch'
+]
+
 export default function Loading() {
   const navigate = useNavigate()
 
   const [progress, setProgress] = useState(0)
+  const [displayText, setDisplayText] = useState()
 
   useEffect(() => {
     const interval = setInterval(() => {
       const value = Math.floor(Math.random() * 100) % 45
 
+      const randomText =
+        displayTexts[Math.floor(Math.random() * displayTexts.length)]
+
+      setDisplayText(randomText)
       setProgress(progress => {
         if (progress + value >= 100) {
           return 100
@@ -42,7 +56,7 @@ export default function Loading() {
         <div style={{ width: (progress * 800) / 100 + 'px' }}></div>
       </div>
 
-      <p className={styles.text}>Connecting to the DOOM Server...</p>
+      <p className={styles.text}>{displayText}</p>
     </div>
   )
 }
